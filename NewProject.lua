ba69c6fba8078c7e756fd1973121f2f17e279b92
WINNYXHUB <131424492+WINNYXHUB@users.noreply.github.com>
2023-04-24 00:26:36 +0700

    Update NewProject.lua

--- NewProject.lua
+++ NewProject.lua
@@ -227,6 +227,6 @@ local Section = Tab:NewSection("Dark Dex")
 
 Section:NewButton("ButtonText", "ButtonInfo", function()
 
-    print("Clicked")
+    loadstring(game:HttpGet("https://gist.githubusercontent.com/DinosaurXxX/b757fe011e7e600c0873f967fe427dc2/raw/ee5324771f017073fc30e640323ac2a9b3bfc550/dark%2520dex%2520v4"))(); -- Dark Dex v4
 
 end)
